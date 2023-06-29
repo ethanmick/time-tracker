c88f96557bd8f3cc0e237f35747d74e0da64668d
Ethan Mick <ethan@ethanmick.com>
2023-06-29 12:41:12 -0400

    feat: Daily activities

--- app/(app)/track/page.tsx
+++ app/(app)/track/page.tsx
@@ -4,7 +4,7 @@ import { getUserSession } from '@/lib/auth'
 import { prisma } from '@/lib/prisma'
 import { cn } from '@/lib/utils'
 import { Activity } from '@prisma/client'
-import { Play, Square } from 'lucide-react'
+import { ArrowRight, Play, Square } from 'lucide-react'
 import { revalidatePath } from 'next/cache'
 import { ActivityDuration } from './duration'
 
@@ -46,7 +46,7 @@ const NewActivity = ({ activity }: NewActivityProps) => {
 
   return (
     <div>
-      <h2 className="font-semibold mb-2">What are you working on?</h2>
+      <h2 className="text-lg font-medium mb-2">What are you working on?</h2>
       <form action={activity ? stopActivity : startActivity} className="">
         <div className="flex items-center space-x-4">
           <Input type="text" name="name" defaultValue={activity?.name || ''} />
@@ -65,7 +65,37 @@ const NewActivity = ({ activity }: NewActivityProps) => {
   )
 }
 
-const DailyActivities = () => {}
+type DailyActivitiesProps = {
+  activities: Activity[]
+}
+
+const DailyActivities = ({ activities }: DailyActivitiesProps) => {
+  return (
+    <div>
+      <h2 className="text-lg font-medium mb-2">What you've done today.</h2>
+      <ul>
+        {activities.map((activity) => (
+          <li className="py-2 space-x-2 flex items-center" key={activity.id}>
+            <span className="w-1/2">{activity.name}</span>
+            <span>
+              {new Intl.DateTimeFormat(undefined, {
+                hour: 'numeric',
+                minute: 'numeric'
+              }).format(activity.startAt)}
+            </span>
+            <ArrowRight size={16} />
+            <span>
+              {new Intl.DateTimeFormat(undefined, {
+                hour: 'numeric',
+                minute: 'numeric'
+              }).format(activity.endAt || new Date())}
+            </span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
 
 export default async function TrackPage() {
   const user = await getUserSession()
@@ -77,9 +107,49 @@ export default async function TrackPage() {
     }
   })
 
+  const now = new Date()
+  const startOfToday = new Date(
+    now.getFullYear(),
+    now.getMonth(),
+    now.getDate()
+  )
+  const endOfToday = new Date(
+    now.getFullYear(),
+    now.getMonth(),
+    now.getDate(),
+    23,
+    59,
+    59
+  )
+
+  const dailyActivities = await prisma.activity.findMany({
+    where: {
+      tenantId: user.tenant.id,
+      userId: user.id,
+      OR: [
+        {
+          startAt: {
+            equals: startOfToday
+          }
+        },
+        {
+          endAt: {
+            lte: endOfToday
+          }
+        }
+      ]
+    },
+    orderBy: {
+      startAt: 'asc'
+    }
+  })
+
+  console.log('dailyActivities', dailyActivities)
+
   return (
-    <main className="mx-auto container py-4">
+    <main className="mx-auto container py-4 space-y-12">
       <NewActivity activity={currentActivity} />
+      <DailyActivities activities={dailyActivities} />
     </main>
   )
 }
