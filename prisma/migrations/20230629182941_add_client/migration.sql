f566e69ab0f6e74a99659974e28a4830784473db
Ethan Mick <ethan@ethanmick.com>
2023-06-29 14:30:44 -0400

    feat: Add db client model

---
+++ prisma/migrations/20230629182941_add_client/migration.sql
@@ -0,0 +1,27 @@
+-- AlterTable
+ALTER TABLE "Activity" ADD COLUMN     "clientId" TEXT,
+ALTER COLUMN "id" SET DEFAULT concat('act_', replace(cast(gen_random_uuid() as text), '-', ''));
+
+-- AlterTable
+ALTER TABLE "Tenant" ALTER COLUMN "id" SET DEFAULT concat('tnt_', replace(cast(gen_random_uuid() as text), '-', ''));
+
+-- AlterTable
+ALTER TABLE "User" ALTER COLUMN "id" SET DEFAULT concat('usr_', replace(cast(gen_random_uuid() as text), '-', ''));
+
+-- CreateTable
+CREATE TABLE "Client" (
+    "id" TEXT NOT NULL DEFAULT concat('cli_', replace(cast(gen_random_uuid() as text), '-', '')),
+    "name" TEXT,
+    "color" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "tenantId" TEXT NOT NULL,
+
+    CONSTRAINT "Client_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Activity" ADD CONSTRAINT "Activity_clientId_fkey" FOREIGN KEY ("clientId") REFERENCES "Client"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Client" ADD CONSTRAINT "Client_tenantId_fkey" FOREIGN KEY ("tenantId") REFERENCES "Tenant"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
