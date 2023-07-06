ecc82adbd4f7f1efe931cbf8e08c0b17a674222b
Ethan Mick <ethan@ethanmick.com>
2023-07-06 14:35:22 -0400

    feat: Update tracking on activity end

--- app/(app)/track/page.tsx
+++ app/(app)/track/page.tsx
@@ -28,25 +28,28 @@ type NewActivityProps = {
 }
 
 const NewActivity = ({ activity, clients, projects }: NewActivityProps) => {
-  async function startActivity(data: FormData) {
+  async function upsertActivity(data: FormData) {
     'use server'
     const user = await getUserSession()
-    await prisma.activity.create({
-      data: {
+    const client = data.get('client') as string
+    const project = data.get('project') as string
+
+    await prisma.activity.upsert({
+      where: {
+        id: data.get('id') as string
+      },
+      create: {
         user: { connect: { id: user.id } },
         tenant: { connect: { id: user.tenant.id } },
         name: data.get('name') as string,
         startAt: new Date(),
-        client: {
-          connect: {
-            id: (data.get('client') as string) ?? undefined
-          }
-        },
-        project: {
-          connect: {
-            id: (data.get('project') as string) ?? undefined
-          }
-        }
+        client: !!client ? { connect: { id: client } } : undefined,
+        project: !!project ? { connect: { id: project } } : undefined
+      },
+      update: {
+        name: data.get('name') as string,
+        client: !!client ? { connect: { id: client } } : undefined,
+        project: !!project ? { connect: { id: project } } : undefined
       }
     })
     revalidatePath('/track')
@@ -54,12 +57,19 @@ const NewActivity = ({ activity, clients, projects }: NewActivityProps) => {
 
   async function stopActivity(data: FormData) {
     'use server'
+
+    const client = data.get('client') as string
+    const project = data.get('project') as string
+
     await prisma.activity.update({
       where: {
         id: data.get('id') as string
       },
       data: {
-        endAt: new Date()
+        endAt: new Date(),
+        name: data.get('name') as string,
+        client: !!client ? { connect: { id: client } } : undefined,
+        project: !!project ? { connect: { id: project } } : undefined
       }
     })
     revalidatePath('/track')
@@ -68,7 +78,7 @@ const NewActivity = ({ activity, clients, projects }: NewActivityProps) => {
   return (
     <div>
       <h2 className="text-lg font-medium mb-2">What are you working on?</h2>
-      <form action={activity ? stopActivity : startActivity} className="">
+      <form action={activity ? stopActivity : upsertActivity} className="">
         <div className="flex items-center space-x-4">
           <Input type="text" name="name" defaultValue={activity?.name || ''} />
           <input type="hidden" name="id" defaultValue={activity?.id || ''} />
