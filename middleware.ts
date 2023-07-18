50e18eecc5ff89f13ca15c29ea3a9188dcc99396
Ethan Mick <ethan@ethanmick.com>
2023-07-18 12:00:37 -0400

    feat: Protect analytics route

--- middleware.ts
+++ middleware.ts
@@ -3,6 +3,7 @@ export { default } from 'next-auth/middleware'
 export const config = {
   matcher: [
     '/admin/:path*',
+    '/analytics/:path*',
     '/clients/:path*',
     '/projects/:path*',
     '/track/:path*'
