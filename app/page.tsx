f2f187651d5fa4eb7c6278086d271a21a2625a43
Ethan Mick <ethan@ethanmick.com>
2024-06-19 13:30:05 -0400

    feat: Retire the project.
    Time Tracker was a fun project that had it's place. Sadly, the energy to
    make it into something bigger and better just wasn't there. If you
    watched my videos, I hope you learned something. That's all I wanted.
    
    Thanks,
    
    - Ethan

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
-import { redirect } from 'next/navigation'
-
 export default function Home() {
-  redirect('/track')
+  return (
+    <main className="flex flex-col justify-center items-center h-screen">
+      <h1 className="font-bold text-4xl">This project has been retired.</h1>
+      <h2 className="font-semibold text-2xl">
+        You can find the source code here:
+      </h2>
+      <a
+        className="text-sky-500 underline"
+        href="https://github.com/ethanmick/time-tracker"
+      >
+        Code
+      </a>
+      <p className="text-lg">Thank you,</p>
+      <p className="text-lg italic">Ethan</p>
+    </main>
+  )
 }
